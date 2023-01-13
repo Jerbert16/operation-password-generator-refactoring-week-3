a9ea1d1844e59d031c14ec770591cb3d418034e6
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-01-12 17:56:23 -0700

    added variables for all characters, added main booleans, added for loop to count password characters to 128 in .js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const lowercaseCharacters =  "abcdefghijklmnopqrstuvwxyz".split("");
+const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const numericTypes = "123456789".split("");
+const specialTypes = [" ", "\"", "\'", "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+const addlowercaseCharacters = true;
+const adduppercaseCharacters = true;
+const addnumericTypes = true;
+const addspecialTypes = true;  
+
+const maxPasswordLength = 129;
+
 // Write password to the #password input
 function writePassword() {
+
+  for (let index = 8; index < maxPasswordLength; index++) {
+    console.log(index);
+  }
+
+  let chooseLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters");
+  
+  confirm("Do you wish to include lowercase types?");
+  
+  confirm("Do you wish to include uppercase types?");
+  
+  confirm("Do you wish to include numeric types?");
+  
+  confirm("Do you wish to include special types?");
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
