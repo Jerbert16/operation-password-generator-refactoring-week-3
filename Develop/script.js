616449d568e21b61d9435afb50f09bce6f4dbf2c
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-01-14 22:33:57 -0700

    created functions for confirms, figured out 8 - 128 range.

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,104 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-const lowercaseCharacters =  "abcdefghijklmnopqrstuvwxyz".split("");
-const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-const numericTypes = "123456789".split("");
-const specialTypes = [" ", "\"", "\'", "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
-
-const addlowercaseCharacters = true;
-const adduppercaseCharacters = true;
-const addnumericTypes = true;
-const addspecialTypes = true;  
-
-const maxPasswordLength = 129;
-
-// Write password to the #password input
-function writePassword() {
-
-  for (let index = 8; index < maxPasswordLength; index++) {
-    console.log(index);
-  }
-
-  let chooseLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters");
-  
-  confirm("Do you wish to include lowercase types?");
-  
-  confirm("Do you wish to include uppercase types?");
-  
-  confirm("Do you wish to include numeric types?");
-  
-  confirm("Do you wish to include special types?");
-
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+var passwordCriteria = {
+lowercaseCharacters: "abcdefghijklmnopqrstuvwxyz".split(""),
+uppercaseCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+numericTypes:"123456789".split(""),
+specialTypes:[
+  " ",
+  '"',
+  "'",
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+],
+passwordLength: 0,
 }
 
+const addLowercaseCharacters = true;
+const addUppercaseCharacters = true;
+const addNumericTypes = true;
+const addSpecialTypes = true;
+
+const randomFunction = {
+  special: getRandomSpecial,
+  upper: getRandomUppercase,
+  lower: getRandomLowercase,
+  number: getRandomNumeric,
+};
+
+function getRandomLowercase() {
+  return passwordCriteria.lowercaseCharacters[Math.floor(Math.random() * passwordCriteria.lowercaseCharacters.length)];
+}
+
+function getRandomUppercase() {
+  return passwordCriteria.uppercaseCharacters[Math.floor(Math.random() * passwordCriteria.uppercaseCharacters.length)];
+}
+
+function getRandomNumeric() {
+  return passwordCriteria.numericTypes[Math.floor(Math.random() * passwordCriteria.numericTypes.length)];
+}
+
+function getRandomSpecial() {
+  return passwordCriteria.specialTypes[Math.floor(Math.random() * passwordCriteria.specialTypes.length)];
+} 
+
+
+
+
+
+
+
+
+
+// --------------------------------------------
+function writePassword() {  
+  var chooseLength = prompt("Choose a password length. Pick number between 8 and 128 to determine final character length.");
+
+  if (chooseLength < 8 || chooseLength > 128 || !isFinite(chooseLength)) {
+    alert("Whoopsie! Invalid entry. Please enter a number between 1 and 128.");
+    return "Your secure password";
+  } else {
+
+  confirm("Do you wish to include lowercase types?");
+
+  confirm("Do you wish to include uppercase types?");
+
+  confirm("Do you wish to include numeric types?");
+
+  confirm("Do you wish to include special types?");
+
+  }
+
+
+  passwordText.value = password;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
