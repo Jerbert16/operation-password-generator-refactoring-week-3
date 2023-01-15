3c1a980d8ea694caaf982d67b0ec4d903348797f
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-01-15 09:20:59 -0700

    reorganized prompts into function, added if/then to make sure a confirm is selected

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,48 @@
 var generateBtn = document.querySelector("#generate");
 var passwordCriteria = {
-lowercaseCharacters: "abcdefghijklmnopqrstuvwxyz".split(""),
-uppercaseCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
-numericTypes:"123456789".split(""),
-specialTypes:[
-  " ",
-  '"',
-  "'",
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "(",
-  ")",
-  "*",
-  "+",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "[",
-  "\\",
-  "]",
-  "^",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
-],
-passwordLength: 0,
-}
-
-const addLowercaseCharacters = true;
-const addUppercaseCharacters = true;
-const addNumericTypes = true;
-const addSpecialTypes = true;
+  lowercaseCharacters: "abcdefghijklmnopqrstuvwxyz".split(""),
+  uppercaseCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+  numericTypes: "123456789".split(""),
+  specialTypes: [
+    " ",
+    '"',
+    "'",
+    "!",
+    "#",
+    "$",
+    "%",
+    "&",
+    "(",
+    ")",
+    "*",
+    "+",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ],
+};
+var passwordLength = 0;
+var lowercase = true;
+var uppercase = true;
+var numericTypes = true;
+var specialTypes = true;
 
 const randomFunction = {
   special: getRandomSpecial,
@@ -53,52 +52,90 @@ const randomFunction = {
 };
 
 function getRandomLowercase() {
-  return passwordCriteria.lowercaseCharacters[Math.floor(Math.random() * passwordCriteria.lowercaseCharacters.length)];
+  return passwordCriteria.lowercaseCharacters[
+    Math.floor(Math.random() * passwordCriteria.lowercaseCharacters.length)
+  ];
 }
 
 function getRandomUppercase() {
-  return passwordCriteria.uppercaseCharacters[Math.floor(Math.random() * passwordCriteria.uppercaseCharacters.length)];
+  return passwordCriteria.uppercaseCharacters[
+    Math.floor(Math.random() * passwordCriteria.uppercaseCharacters.length)
+  ];
 }
 
 function getRandomNumeric() {
-  return passwordCriteria.numericTypes[Math.floor(Math.random() * passwordCriteria.numericTypes.length)];
+  return passwordCriteria.numericTypes[
+    Math.floor(Math.random() * passwordCriteria.numericTypes.length)
+  ];
 }
 
 function getRandomSpecial() {
-  return passwordCriteria.specialTypes[Math.floor(Math.random() * passwordCriteria.specialTypes.length)];
-} 
-
-
-
-
-
-
-
-
+  return passwordCriteria.specialTypes[
+    Math.floor(Math.random() * passwordCriteria.specialTypes.length)
+  ];
+}
 
 // --------------------------------------------
-function writePassword() {  
-  var chooseLength = prompt("Choose a password length. Pick number between 8 and 128 to determine final character length.");
 
-  if (chooseLength < 8 || chooseLength > 128 || !isFinite(chooseLength)) {
+function writePassword() {
+  var chooseLength = prompt(
+    "Choose a password length. Pick number between 8 and 128 to determine final character length."
+  );
+
+  if (chooseLength === null) {
+    return;
+  } else if (
+    chooseLength < 8 ||
+    chooseLength > 128 ||
+    !isFinite(chooseLength)
+  ) {
     alert("Whoopsie! Invalid entry. Please enter a number between 1 and 128.");
-    return "Your secure password";
-  } else {
-
-  confirm("Do you wish to include lowercase types?");
-
-  confirm("Do you wish to include uppercase types?");
-
-  confirm("Do you wish to include numeric types?");
-
-  confirm("Do you wish to include special types?");
-
+    return;
   }
 
+  pwConfirms();
+
+  if (lowercase === false && uppercase === false && numericTypes === false && specialTypes === false) {
+    alert("You must select at least oneof the folloeing: lowercase, uppercase, number or special characters");
+    pwConfirms();
+  }
+  
+    if (lowercase === true && passwordCriteria.passwordLength < chooseLength) {
+      console.log("yay");
+    } else {
+      console.log("nah");
+    }
+
+    if (uppercase === true) {
+      console.log("yay");
+    } else {
+      console.log("nah");
+    }
+
+    if (numericTypes === true) {
+      console.log("yay");
+    } else {
+      console.log("nah");
+    }
+
+    if (specialTypes === true) {
+      console.log("yay");
+    } else {
+      console.log("nah");
+    }
+
+  
 
   passwordText.value = password;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 }
+
+function pwConfirms() {
+  lowercase = confirm("Do you wish to include lowercase types?");
+  uppercase = confirm("Do you wish to include lowercase types?");
+  numericTypes = confirm("Do you wish to add numbers?");
+  specialTypes = confirm("Do you wish to include special types?");
+}
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
